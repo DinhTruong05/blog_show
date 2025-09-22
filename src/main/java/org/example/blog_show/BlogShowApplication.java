764fe5e8183c12574c64f6d145164bafbf2c3d5e
DinhTruong05 <dinhtruong051095@gmai.com>
2025-09-22 15:16:19 +0700

    create

---
+++ src/main/java/org/example/blog_show/BlogShowApplication.java
@@ -0,0 +1,13 @@
+package org.example.blog_show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogShowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/blog_show/BlogShowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.blog_show;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogShowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
